c8ac995ad807f7440fd8bec3066a76b01164c4df
Abdulhay004 <abdulhaytolibjonov8@gmail.com>
2024-11-03 00:17:42 +0500

    any

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,44 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+from django.contrib.auth.decorators import user_passes_test
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+       openapi.Info(
+           title="My API",
+           default_version='v1',
+           description="Test description",
+           terms_of_service="https://www.google.com/policies/terms/",
+           contact=openapi.Contact(email="contact@myapi.local"),
+           license=openapi.License(name="BSD License"),
+       ),
+       public=True,
+       permission_classes=(permissions.AllowAny,),
+   )
+
+
+
+
+def is_superuser(user):
+    # return user.is_superuser    # faqat superuserlar ko'ra oladi
+    return user.is_authenticated
+    # return True qilinsa istalgan user kira oladi
+
+# urllarninig classlarini to'g'irlab chiqing schema, swagger, redoc
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('schema/', user_passes_test(is_superuser)(SpectacularAPIView.as_view()), name='schema'),
+    path('swagger/', user_passes_test(is_superuser)(SpectacularSwaggerView.as_view()), name='swagger-ui'),
+    path('redoc/', user_passes_test(is_superuser)(SpectacularRedocView.as_view()), name='redoc'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
