a11a9350975ae5987a4041a34af7b3ed052d9eee
Abdulhay004 <abdulhaytolibjonov8@gmail.com>
2024-12-13 20:09:18 +0500

    any

--- apps/notification/urls.py
+++ apps/notification/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import NotificationAPIView
+from .views import NotificationAPIView, NotificationDetailAndUpdateView
 
 urlpatterns = [
     path('', NotificationAPIView.as_view(), name='notifications'),
+    path('<uuid:id>/', NotificationDetailAndUpdateView.as_view(), name='notifications-detail'),
 ]
--- apps/notification/views.py
+++ apps/notification/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Notification
 from django.db.models import Count
 from datetime import datetime
+from .serializers import NotificationSerializer
 
 
 class NotificationAPIView(generics.ListAPIView):
@@ -32,3 +33,33 @@ class NotificationAPIView(generics.ListAPIView):
     def get_serializer_class(self):
         from .serializers import NotificationSerializer
         return NotificationSerializer
+
+class NotificationDetailAndUpdateView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = NotificationSerializer
+
+    def get_object(self):
+        try:
+            notification = Notification.objects.get(id=self.kwargs['id'], user=self.request.user)
+            return notification
+        except Notification.DoesNotExist:
+            return Response({'detail': 'Notification not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+    def retrieve(self, request, *args, **kwargs):
+        notification = self.get_object()
+        serializer = self.get_serializer(notification)
+        return Response(serializer.data)
+
+
+    def update(self, request, *args, **kwargs):
+        groups = request.user.groups.first()
+        if groups == None:
+            return Response(status=403)
+        notification = self.get_object()
+        serializer = self.get_serializer(notification, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
